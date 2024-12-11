5968c714e4efcfe0a67dd39ec08fffa1c946fddc
Katie <ktpham115@student.hbuhsd.edu>
2024-12-11 21:02:50 +0000

    4.1 1st day

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-  // write code here
-  
+  int age = 17;
+  double price = 19.99;
+  bool isRaining = false;
+
+  std::cout << age << "\n" << price << std::endl << isRaining <<"\n";
+
+  int grade = 11;
+  std::cout << "I am an" << " " << grade << "th grader." <<"\n";
   return 0;
 }
